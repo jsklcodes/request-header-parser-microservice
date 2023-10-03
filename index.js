12e9106bdd9b279bbd5fb9f358f2238368c86eb6
Kallyne Layane <hellookallyne@gmail.com>
2023-10-03 15:38:40 -0300

    Add whoami endpoint

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.set('trust proxy', true);
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -22,6 +24,15 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// whoami endpoint
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+  });
 });
 
 // listen for requests :)
